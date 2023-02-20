3a8c87b1638c18a2290286e9da58c42332bb7d39
Radu Lepadatu <radulepa@gmail.com>
2023-02-20 22:46:02 +0200

    Create Company Model
    'name', 'address', 'city', 'state', 'zip'

---
+++ origindata-interview/app/Models/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'address', 'city', 'state', 'zip'
+    ];
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+}
