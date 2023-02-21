6560b5060a8bbf577fb38147c6e31188d37564dd
Radu Lepadatu <radulepa@gmail.com>
2023-02-21 13:34:26 +0200

    Created CompanyController with CRUD functionalities and Auth

---
+++ origindata-interview/app/Http/Controllers/CompanyController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $companies = Company::all();
+        return response()->json($companies);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'description' => 'nullable',
+            'created_at' => 'nullable|date',
+            'founded_at' => 'nullable|date',
+            'updated_at' => 'nullable|date'
+        ]);
+
+        $company = new Company();
+        $company->name = $validatedData['name'];
+        $company->description = $validatedData['description'];
+        $company->founded_at = $validatedData['founded_at'];
+        $company->save();
+
+        return response()->json(['message' => 'Company created successfully', 'company' => $company], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $company = Company::findOrFail($id);
+
+        if ($company)
+            return response()->json($company);
+
+        return response()->json(['message' => 'Company not found'], 404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = Auth::user();
+
+        $company = Company::findOrFail($id);
+        $company->update($request->all());
+        return response()->json($company, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = Auth::user();
+
+        $company = Company::find($id);
+        if (!$company) {
+            return response()->json(['message' => 'Company not found'], 404);
+        }
+
+        $company->delete();
+        return response()->json(['message' => 'Company deleted'], 200);
+    }
+}
