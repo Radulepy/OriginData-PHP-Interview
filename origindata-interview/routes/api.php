65fdbfde678c5aea1eb583cc9f3fea9183f306a7
Radu Lepadatu <radulepa@gmail.com>
2023-02-21 13:36:17 +0200

    Update Routes, divide the middleware from get

--- origindata-interview/routes/api.php
+++ origindata-interview/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//* TEST ROUTE: 
+Route::get('/test', function () {
+    return response()->json(['success' => 'Hello!']);
+});
+
+//* Company GET routes:
+Route::get('/companies', 'App\Http\Controllers\CompanyController@index');
+Route::get('/companies/{id}', 'App\Http\Controllers\CompanyController@show');
+
+//* Employee GET routes:
+Route::get('/employee', 'App\Http\Controllers\EmployeeController@index');
+Route::get('/employee/{id}', 'App\Http\Controllers\EmployeeController@show');
+
+//* Project GET routes:
+Route::get('/project', 'App\Http\Controllers\ProjectController@index');
+Route::get('/project/{id}', 'App\Http\Controllers\ProjectController@show');
+
+//TODO: api versioning (prefix->('v1')), now no versioning is done: 
+Route::middleware('auth:api')->group(function () {
+
+    //* Authentication only: Create, Update and Delet
+
+    // Company Model
+    Route::post('/companies', 'App\Http\Controllers\CompanyController@store');
+    // PUT:
+    Route::put('/companies/{id}', 'App\Http\Controllers\CompanyController@update');
+    // DELETE:
+    Route::delete('/companies/{id}', 'App\Http\Controllers\CompanyController@destroy');
+
+    // Employee Model
+    Route::post('/employee', 'App\Http\Controllers\EmployeeController@store');
+    // PUT:
+    Route::put('/employee/{id}', 'App\Http\Controllers\EmployeeController@update');
+    // DELETE:
+    Route::delete('/employee/{id}', 'App\Http\Controllers\EmployeeController@destroy');
+
+    // Project Model
+    Route::post('/project', 'App\Http\Controllers\ProjectController@store');
+    // PUT:
+    Route::put('/project/{id}', 'App\Http\Controllers\ProjectController@update');
+    // DELETE:
+    Route::delete('/project/{id}', 'App\Http\Controllers\ProjectController@destroy');
+});
+
+//* Routes to register or login a user:
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::post('/register', 'App\Http\Controllers\AuthController@register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
